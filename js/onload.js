fb964972334f44169903e3f444e5e4c055e83002
Nat Welch <nat@natwelch.com>
2009-12-18 23:07:10 -0800

    Made some style changes.
    I'll be fully redoing the css soon.

--- js/onload.js
+++ js/onload.js
@@ -1,8 +1,7 @@
 window.addEvent('domready', function() {
    $$('.collapse').each(function(el, index) {
-      console.log(el);
       var content = el.getElement('div');
-      var title = el.getElement('h1');
+      var title = el.getElement('h2');
       var mySlide = new Fx.Reveal(content, {
          duration: 'short',
          tansition: 'linear'
