afe420cba4918d313f9f59df2e7ac688e6341f64
Nat Welch <nat@natwelch.com>
2010-05-09 19:09:16 -0700

    Have the publish script actually do something.

--- publish.sh
+++ publish.sh
@@ -7,5 +7,9 @@ POSTS=`ls _drafts/ | grep '-'`;
 select opt in $POSTS; do
    echo "Publishing: $opt.";
    echo "git mv _drafts/$opt _posts/"
+   git mv _drafts/$opt _posts/
+   echo "git ci _drafts/$opt _posts/$opt -m \"Publishing $opt.\"";
+   git ci _drafts/$opt _posts/$opt -m "Publishing $opt."
    exit 0;
 done;
+
