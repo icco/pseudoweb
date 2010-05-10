775930ba6f26d2c9bb93903a92dbf4ffc3210faf
Nat Welch <nat@natwelch.com>
2010-05-09 18:02:23 -0700

    updates to new post.
    Fixes title conversion and adds time started to post

--- new_post.sh
+++ new_post.sh
@@ -2,8 +2,9 @@
 
 echo -n "Post title? ";
 read -e TITLE;
-DTITLE=`echo -n $TITLE | sed 's/ /-/g' | tr "[:upper:]" "[:lower:]"`;
+DTITLE=`echo -n $TITLE | sed 's/ /-/g' | sed 's/[^A-Za-z0-9\-]//g' | tr "[:upper:]" "[:lower:]"`;
 DATE=`date +%Y-%m-%d`;
+TIME=`date +%H:%M:%S`;
 FILENAME=_drafts/$DATE-$DTITLE.md
 
 if [ -f $FILENAME ]; then
@@ -15,6 +16,7 @@ else
    echo "layout: post" >> $FILENAME
    echo "title:" $TITLE >> $FILENAME
    echo "location:" $LOC >> $FILENAME
+   echo "time: " $TIME >> $FILENAME
    echo "---" >> $FILENAME
 fi
 
