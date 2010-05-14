2f74ff204b80350f124a00881725dcca75e3cfce
Nat Welch <nat@natwelch.com>
2010-05-14 15:08:53 -0700

    Adds exit state to publish

--- publish.sh
+++ publish.sh
@@ -2,9 +2,11 @@
 # Lists the posts to publish, then updates them and sends them live
 # by Nat Welch 2010
 
-POSTS=`ls _drafts/ | grep '-'`;
+POSTS=`ls _drafts/ | grep '-' && echo 'exit'`;
 
 select opt in $POSTS; do
+   if [ $opt == 'exit' ]; then exit 0; fi;
+
    echo "Publishing: $opt.";
    echo "git mv _drafts/$opt _posts/"
    git mv _drafts/$opt _posts/
