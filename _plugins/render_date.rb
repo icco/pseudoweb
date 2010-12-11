d58eebfafb33c0c467c1da7a13b1fd314139610b
Nat Welch <nat@natwelch.com>
2010-12-11 15:15:25 -0800

    Plugin attempts. Untested.

---
+++ _plugins/render_date.rb
@@ -0,0 +1,16 @@
+# From the Jekyll wiki.
+module Jekyll
+  class RenderTimeTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end
+
+    def render(context)
+      "#{@text} #{Time.now}"
+    end
+  end
+end
+
+Liquid::Template.register_tag('render_time', Jekyll::RenderTimeTag)
